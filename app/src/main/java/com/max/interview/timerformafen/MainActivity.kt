b3d98b4c5fa5d98967c016a0d87a49632587c8fe
BuenasDias <tmaksim1989@gmail.com>
2022-03-13 21:58:11 +0300

    Check

---
+++ app/src/main/java/com/max/interview/timerformafen/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.max.interview.timerformafen
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.max.interview.timerformafen.databinding.ActivityMainBinding
+
+@SuppressLint("SetTextI18n")
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    private val viewModel : MainActivityVIewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+        initObservers()
+    }
+
+    private fun initObservers() {
+
+        with(viewModel){
+            second.observe(this@MainActivity){ updateText(it) }
+
+            isPause.observe(this@MainActivity){
+                if(!it)binding.btnStart.text = "pause timer"
+                else binding.btnStart.text = "start timer"
+            }
+        }
+
+    }
+
+    private fun updateText(value : Any?) {
+        binding.textTimer.text = value.toString()
+    }
+
+    private fun initView() {
+        with(binding){
+            btnStart.setOnClickListener {
+                if(viewModel.isPause.value!!){
+                    viewModel.createTimer()
+                } else {
+                    viewModel.pauseTimer()
+                }
+            }
+
+            btnStop.setOnClickListener {
+                viewModel.stopTimer()
+            }
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/max/interview/timerformafen/MainActivityVIewModel.kt
@@ -0,0 +1,61 @@
+package com.max.interview.timerformafen
+
+import android.os.CountDownTimer
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityVIewModel : ViewModel() {
+
+    private var timer: CountDownTimer? = null
+
+    private val _seconds: MutableLiveData<Long> = MutableLiveData(0)
+    var second: LiveData<Long> = _seconds
+
+    private val _isTimer: MutableLiveData<Boolean> = MutableLiveData(false)
+    var isTimer: LiveData<Boolean> = _isTimer
+
+    private val _isPause: MutableLiveData<Boolean> = MutableLiveData(true)
+    var isPause: LiveData<Boolean> = _isPause
+
+    val timerOriginal = 10000L
+
+    private val _timerCurrent: MutableLiveData<Long> = MutableLiveData(10000L)
+    var timerCurrent: LiveData<Long> = _timerCurrent
+
+
+    fun createTimer() {
+
+        Log.d("TAG", "createTimer: true")
+
+        _isTimer.postValue(true)
+        _isPause.postValue(false)
+
+        timer = object : CountDownTimer(_timerCurrent.value!!, 1000) {
+            override fun onTick(p0: Long) {
+                _seconds.postValue(p0 / 1000)
+            }
+
+            override fun onFinish() {
+                _isTimer.postValue(false)
+                _isPause.postValue(true)
+                _timerCurrent.postValue(timerOriginal)
+            }
+        }.start()
+    }
+
+    fun pauseTimer(){
+        timer?.cancel()
+        _isPause.postValue(true)
+        _timerCurrent.postValue(_seconds.value!!)
+    }
+
+    fun stopTimer() {
+        timer?.cancel()
+        _isTimer.postValue(false)
+        _isPause.postValue(true)
+        _seconds.postValue(0L)
+    }
+
+}
